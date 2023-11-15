5915c7ce1642355934a699dd7c4ab54c5b40a895
mohamed-ibrahim-21 <m.ibrahim21800@gmail.com>
2023-11-15 19:33:46 +0200

    make main project on mp lap application.c & application.h & add gpio & add eculayer LCD-KEYPAD & and make project on proteus

---
+++ Calculator project.X/application.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   application.h
+ * Author: mohamed ibrahim
+ *
+ * Created on October 4, 2023, 10:21 PM
+ */
+
+#ifndef APPLICATION_H
+#define	APPLICATION_H
+
+/* Section : Includes */
+#include "ECU_Layer/Keypad/ecu_keypad.h"
+#include "ECU_Layer/LCD/ecu_lcd.h"
+/* Section : Macro Declarations */
+
+/* Section : Macro Functions Declarations*/
+
+/* Section : Data Type Declarations*/
+
+/* Section : Functions Declarations*/
+void application_intialize(void);
+
+
+#endif	/* APPLICATION_H */
+
---
+++ Calculator project.X/build/default/debug/application.p1.d
@@ -0,0 +1,13 @@
+build/default/debug/application.p1:  \
+application.c  \
+application.h  \
+ECU_Layer/Keypad/ecu_keypad.h  \
+ECU_Layer/Keypad/ecu_keypad_cfg.h  \
+ECU_Layer/Keypad/../../MCAL_Layer/GPIO/hal_gpio.h  \
+ECU_Layer/Keypad/../../MCAL_Layer/GPIO/../mcal_std_types.h  \
+ECU_Layer/Keypad/../../MCAL_Layer/GPIO/../std_libraries.h  \
+ECU_Layer/Keypad/../../MCAL_Layer/GPIO/../compiler.h  \
+ECU_Layer/Keypad/../../MCAL_Layer/GPIO/../device_config.h  \
+ECU_Layer/Keypad/../../MCAL_Layer/GPIO/hal_gpio_cfg.h  \
+ECU_Layer/LCD/ecu_lcd.h  \
+ECU_Layer/LCD/ecu_lcd_cfg.h 
